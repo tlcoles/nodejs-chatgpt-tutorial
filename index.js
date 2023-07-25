cb9c6133da7376b63bee4ca1bacdab735034c2c9
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2023-07-25 14:32:11 +0200

    create interactive UI in the terminal

--- index.js
+++ index.js
@@ -9,14 +9,24 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-openai
-  .createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "Hello" }],
-  })
-  .then((res) => {
-    console.log(res.data.choices[0].message.content);
-  })
-  .catch((e) => {
-    console.log(e);
-  });
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+userInterface.prompt();
+
+userInterface.on("line", async (input) => {
+    await openai
+    .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: input }],
+    })
+    .then((res) => {
+        console.log("AI: ", res.data.choices[0].message.content);
+        userInterface.prompt();
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+});
