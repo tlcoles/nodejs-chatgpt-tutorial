749726cafb0d1202a1a2b80dcedad798fbb0b92a
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2023-07-25 13:38:41 +0200

    test response of ChatGPT

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ import "dotenv/config";
 import { Configuration, OpenAIApi } from "openai";
 import readline from "readline";
 
+const configuration = new Configuration({
+    organization: process.env.OPENAI_ORGANIZATION,
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+const openai = new OpenAIApi(configuration);
+
+openai
+  .createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Hello" }],
+  })
+  .then((res) => {
+    console.log(res.data.choices[0].message.content);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
